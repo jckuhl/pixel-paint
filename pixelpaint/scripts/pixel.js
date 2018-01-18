60e7ba8c534c80bc2b89b1aeee62f6389b19eb69
Jonathan Kuhl <jckuhl87@gmail.com>
2018-01-18 00:40:29 -0500

    Add files via upload

--- pixelpaint/scripts/pixel.js
+++ pixelpaint/scripts/pixel.js
@@ -5,15 +5,53 @@
 	const body = document.querySelector("body");
 	const warning = document.getElementById("warning");
 	const warningBtn = document.getElementById("warning-btn");
+	const resetBtn = document.getElementById("reset");
+	const randomBtn = document.getElementById("random-color");
+	const colorPicker = document.getElementById("color-picker");
 
+	let gridArray = [];
+	let currentColor = "#2f999e";
+
+
+	function random(min, max) {
+   		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
 	function changeColor() {
-		this.style.backgroundColor = "black";
+		this.style.backgroundColor = currentColor;
 	}
+
+	function hex(rgb) {
+		let hex = rgb.toString(16);
+		return hex.length == 1 ? "0" + hex : hex;
+	}
+
+	colorPicker.addEventListener("change", ()=> {
+		currentColor = colorPicker.value;
+	});
+
+	resetBtn.addEventListener("click", ()=> {
+		gridsizeInput.value = "";
+		if(gridArray !== []) {
+			gridArray.forEach( (sq)=> sq.style.backgroundColor = "white");
+		}
+	});
+
+	randomBtn.addEventListener("click", ()=> {
+		let red = random(0, 255);
+		let green = random(0, 255);
+		let blue =random(0, 255);
+
+		currentColor = `rgb(${red}, ${green}, ${blue})`;
+
+		colorPicker.value = `#${hex(red)}${hex(green)}${hex(blue)}`;
+	});
 
 	createBtn.addEventListener("click",()=> {
 		const gridsize = parseInt(gridsizeInput.value);
-		if((!isNaN(gridsize)) && (gridsize > 0) && (gridsize < 11)){
+		if((!isNaN(gridsize)) && (gridsize > 0) && (gridsize < 21)){
+			gridArray.forEach( (sq)=> body.removeChild(sq) );
+			gridArray = [];
 			const squareWidth = 500 / gridsize;
 			const numSquares = gridsize * gridsize;
 			let pos_y = playArea.getBoundingClientRect().top;
@@ -28,6 +66,7 @@
 				square.style.height = squareWidth + "px";
 				body.appendChild(square);
 				square.addEventListener("click",changeColor);
+				gridArray.push(square);
 				pos_x += squareWidth;
 				row++
 				if(Math.pow(row,2) === numSquares) {
