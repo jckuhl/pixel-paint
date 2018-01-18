bef2042bfbc0fec9c5fde2aea3e53985ebf88910
Jonathan Kuhl <jckuhl87@gmail.com>
2018-01-17 21:02:13 -0500

    Add files via upload
    Initial Commit

---
+++ pixelpaint/scripts/pixel.js
@@ -0,0 +1,18 @@
+(function() {
+	const playArea = document.getElementById("playArea");
+	const createBtn = document.getElementById("create");
+	const gridsizeInput = document.getElementById("gridsize");
+
+	createBtn.addEventListener("click",()=> {
+		const gridsize = gridsizeInput.value;
+		if((gridsize !== 0) || (gridsize !== "")) {
+			const squareWidth = 500 / gridsize;
+			const numSquares = gridsize * gridsize;
+		} else {
+
+		}	
+	})
+
+})();
+
+
